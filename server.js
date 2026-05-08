fc8f638b925c1f7d903463ac0e659b30069c069c
Ashkar S <ashkars178@gmail.com>
2026-05-08 10:07:38 +0530

    Fix CORS for correct Vercel domain

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://entra-gjvt.vercel.app'
+    'https://entra-orcin.vercel.app'
   ],
   credentials: true
 }));
